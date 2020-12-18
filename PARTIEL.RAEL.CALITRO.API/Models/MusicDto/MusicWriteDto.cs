ad8b5ca1a1930d5b4367abd792ca603c102a437a
rael06 <rael06@hotmail.fr>
2020-12-18 11:00:09 +0100

    add controllers

--- PARTIEL.RAEL.CALITRO.API/Models/MusicDto/MusicWriteDto.cs
+++ PARTIEL.RAEL.CALITRO.API/Models/MusicDto/MusicWriteDto.cs
@@ -6,5 +6,7 @@ namespace PARTIEL.RAEL.CALITRO.API.Models.MusicDto
         public int Id { get; init; }
         public string Title { get; init; }
         public string Url { get; init; }
+
+        public int ArtistId { get; init; }
     }
 }
